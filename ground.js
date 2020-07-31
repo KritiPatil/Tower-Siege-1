d13481cc392a8ca7e07bbd7fe648d7991ff56784
KritiPatil <66488628+KritiPatil@users.noreply.github.com>
2020-07-31 12:02:08 -0700

    Add files via upload

--- ground.js
+++ ground.js
@@ -1,22 +1,23 @@
-class Block{
-    constructor(x, y, width, height, color) {
+class Ground{
+    constructor(x, y, width, height) {
         var options = {
-          'isStatic': false,
+            isStatic : true,
             'restitution':0.8,
             'friction':1.0,
-            'density':0.2
+            'density':1.0
         }
-        
+
+        strokeWeight (2);
+        stroke ("white");
         this.body = Bodies.rectangle(x, y, width, height, options);
         World.add(world, this.body);
         this.width = width;
         this.height = height;
-        this.color = color;
       }
       show(){
         var angle = this.body.angle;
         push();
-        fill (this.color);
+        fill ("white");
         translate(this.body.position.x, this.body.position.y);
         rotate(angle);
         rect(0, 0, this.width, this.height);
